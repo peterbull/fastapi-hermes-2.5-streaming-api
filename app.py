b8db3eb4903e777f8ab6419770a8d45740d69441
LAPTOP-VCGCOL4V <petercbull@gmail.com>
2023-11-08 11:57:10 -0500

    add inference test nbs

--- app.py
+++ app.py
@@ -9,11 +9,41 @@ from sse_starlette import EventSourceResponse
 
 # Load model
 print("Loading model...")
-llm = Llama(model_path="./model/llama-2-13b-chat.ggmlv3.q4_1.bin")
+llm = Llama(model_path="./model/openhermes-2.5-mistral-7b.Q4_0.gguf")
 print("Model loaded!")
 
 app = FastAPI()
 
 @app.get("/llama")
 async def llama(request: Request, question: str):
-    
+    stream = llm(
+        f"""{question}""",
+        max_tokens=100,
+        stop=["\n", " Q:"],
+        stream=True,
+    )
+
+    async def async_generator():
+        for item in stream:
+            yield item
+
+    async def server_sent_events():
+        async for item in async_generator():
+            if await request.is_disconnected():
+                break
+            result = copy.deepcopy(item)
+            text = result["choices"][0]["text"]
+            yield {"data": text}
+    return EventSourceResponse(server_sent_events())
+
+
+@app.get("/llamastatic")
+async def llama(request: Request, question: str):
+    res = llm(
+        f"""{question}""",
+        max_tokens=100,
+        stop=["\n", " Q:"],
+        stream=False,
+    )
+
+    return res
