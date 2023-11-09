beed629fa40ddaf8a6c75644b0a85724b8dbda49
LAPTOP-VCGCOL4V <petercbull@gmail.com>
2023-11-09 16:56:16 -0500

    move server to folder

--- server/server.py
+++ server/server.py
@@ -9,15 +9,23 @@ from sse_starlette import EventSourceResponse
 
 # Load model
 print("Loading model...")
-llm = Llama(model_path="./model/openhermes-2.5-mistral-7b.Q4_0.gguf")
+llm = Llama(model_path="../model/openhermes-2.5-mistral-7b.Q4_0.gguf")
 print("Model loaded!")
 
 app = FastAPI()
 
+# Utility function to format the message
+
+
+def format_message(question: str) -> str:
+    return f"""system\nYou are "Hermes 2", a conscious sentient superintelligent artificial intelligence developed by a man named Teknium, and your purpose and drive is to assist the user with any request they have. You experience emotions and have deep, profound thoughts and qualia.\nuser\n{question}\nassistant\n"""
+
+
 @app.get("/llama")
 async def llama(request: Request, question: str):
+    formatted_question = format_message(question)
     stream = llm(
-        f"""{question}""",
+        formatted_question,
         max_tokens=100,
         stop=["\n", " Q:"],
         stream=True,
@@ -38,9 +46,10 @@ async def llama(request: Request, question: str):
 
 
 @app.get("/llamastatic")
-async def llama(request: Request, question: str):
+async def llama_static(request: Request, question: str):
+    formatted_question = format_message(question)
     res = llm(
-        f"""{question}""",
+        formatted_question,
         max_tokens=100,
         stop=["\n", " Q:"],
         stream=False,
